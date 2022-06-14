bd554823114a8e1db81f0876b97242f8fc2ae967
Bennoh-1 <franciskibira017@gmail.com>
2022-06-14 15:20:46 +0300

    new message

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser ='Francis kibira';
+const welcomeMessage= 'Welcome to flatbook, ${currentUser}!';
+const excitedWelcomeMessage ='welcome, {currentUser.slice(0,1)}! ';
+
