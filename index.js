ab0570ea412c2661c0db2f6b49bb57fa5f2cf55b
Bennoh-1 <franciskibira017@gmail.com>
2022-06-25 07:03:12 +0300

    commit tree 98c4d4607edabf3c07aa190b8e074c412190c12a

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code in this file!
-const currentUser ='Francis kibira';
-const welcomeMessage= 'Welcome to flatbook, ${currentUser}!';
-const excitedWelcomeMessage ='welcome, {currentUser.slice(0,1)}! ';
+const currentUser = 'Francis Kibira';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
 
